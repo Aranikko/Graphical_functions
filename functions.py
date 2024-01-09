c57166dbdc3baec4e72cd49b0663201a473d8be1
Aranikko <gametv3020@gmail.com>
2024-01-09 17:14:22 +0300

    1.0

--- functions.py
+++ functions.py
@@ -1,27 +1,51 @@
-x = []
-y = []
-k = 1
-b = 0
-
 import matplotlib.pyplot as plt
-from math import *
-
-def linear(x, k, b):
-    x_list = list(map(int, filter(None, x.value.split(','))))
-            
-    plt.style.use('dark_background')
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+def linear(x_values: list[str], k:str, b:str, theme:str):
+    x_list = list(map(int, x_values.split(',')))
     
-    y = []
+    plt.plot(x, y)
 
-    for i in range(len(x_list)):
-        y.append(int(k.value) * x_list[i] + int(b.value))
-
-    plt.plot(x_list, y, label=' ')
-    plt.title(' ')
-    plt.xlabel('Ось X')
-    plt.ylabel('Ось Y')
+    y = [int(k) * x + int(b) for x in x_list]
+    if theme == "black":
+        plt.style.use('dark_background')
+    else:
+        pass
+    plt.plot(x_list, y, label='Linear Function')
+    plt.title('Linear Function')
+    plt.xlabel('X Axis')
+    plt.ylabel('Y Axis')
     plt.legend()
     plt.savefig('Graph.png')
+    plt.clf()  # Clear the current figure after saving the graph
+
+def quadratic(x_values, n):
+    x_list = list(map(int, x_values.split(',')))
     
+    y = [x**n for x in x_list]
+
+    plt.style.use('dark_background')
+    plt.plot(x_list, y, label='Quadratic Function')
+    plt.title('Quadratic Function')
+    plt.xlabel('X Axis')
+    plt.ylabel('Y Axis')
+    plt.legend()
+    plt.savefig('Quadratic_Graph.png')
+    plt.clf()  # Clear the current figure after saving the graph
+
+
+
+
+
+
+
+plt.title('Простой линейный график')
+plt.xlabel('Ось X')
+plt.ylabel('Ось Y')
+
+
+
+
+plt.show()
 
 
--- main.py
+++ main.py
@@ -1,21 +1,76 @@
-import PySimpleGUI as sg
+import customtkinter as ctk
+from PIL import Image
+import functions
+app = ctk.CTk()
+app.geometry("810x484")
+app.resizable(False, False)
 
-# Define the window's contents
-layout = [[sg.Text("K"), sg.DropDown("y=kx+b, y=gb", size=10)],
-          [sg.Input(key='-INPUTK-', values=''), sg.Input(key='-INPUTX-'), sg.Input(key='-INPUTB-')],
-          [sg.Button('submit')]]
 
-# Create the window
-window = sg.Window('Window Title', layout)
+def select_theme(choise):
+    if choise == "white":
+        ctk.set_appearance_mode("light")
+        print("white")
+        app.update()
+    else:
+        ctk.set_appearance_mode("dark")
 
-# Display and interact with the Window using an Event Loop
-while True:
-    event, values = window.read()
-    # See if user wants to quit or window was closed
-    if event == sg.WINDOW_CLOSED:
-        break
-    # Output a message to the window
-    print(values['-INPUTX-'])
+
+theme_dd = ctk.CTkComboBox(app, values=['black', 'white'], command=select_theme, state='readonly')
+theme_dd.place(relx=0.821, rely=0.025)
+theme_dd.set("Select theme")
+
+my_image = ctk.CTkImage(light_image=Image.open("Graph.png"),
+                                  dark_image=Image.open("Graph.png"),
+                                  size=(640, 480))
+
+image_label = ctk.CTkLabel(app, image=my_image, text="") 
+image_label.place(relx=0.005, rely=0)
+
+x =ctk.CTkEntry(app, placeholder_text="x:")
+k =ctk.CTkEntry(app, placeholder_text="k:")
+b =ctk.CTkEntry(app, placeholder_text="b:")
+
+def submit():
+    global image_label, my_image
     
-# Finish up by removing from the screen
-window.close()
+    image_label.place_forget()
+    
+    functions.linear(x.get(), k.get(), b.get(), theme_dd.get())
+    
+    my_image = ctk.CTkImage(light_image=Image.open("Graph.png"),
+                                  dark_image=Image.open("Graph.png"),
+                                  size=(640, 480))
+    
+    image_label = ctk.CTkLabel(app, image=my_image, text="") 
+    
+    image_label.place(relx=0.005, rely=0)
+
+    pass
+
+submit_b=ctk.CTkButton(app, text="Submit", command=submit)
+
+def combobox_callback(choice):
+    if choice == "y=kx+b":
+
+        x.place(relx=0.821, rely=0.175)
+        
+        k.place(relx=0.821, rely=0.250)
+        
+        b.place(relx=0.821, rely=0.325)
+        
+        submit_b.place(relx=0.821, rely=0.400)
+    else:
+        x.place_forget()
+        k.place_forget()
+        b.place_forget()
+        submit_b.place_forget()
+        
+
+# Create a list of options for the options
+options = ["y=kx+b", "y=x^n", "Option 3"]
+
+# Create the CTkCombobox widgeta
+options = ctk.CTkComboBox(app,state='readonly', values=options,command=combobox_callback)
+options.place(relx=0.821, rely=0.100)
+options.set("Select")
+app.mainloop()
