ff675189f95336be752b202542084ad1021ab960
Aranikko <gametv3020@gmail.com>
2024-02-03 15:18:59 +0300

    1.125
    add new func

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import os
+from matplotlib import cm
+import numpy as np
 plt.switch_backend('Agg')
 
 def linear(x_values: list[str], k:str, b:str, theme:str):
@@ -55,3 +57,35 @@ def quadratic(x_values, n):
 # quadratic('4,3,1,0,-1,-3,-4', '2')
 
 
+def plot_3d_surface(x_values, y_values, theme='default'):
+    """
+    Plots a 3D surface graph based on x, y values, with z being the product of x and y.
+
+    Parameters:
+    - x_values: List of x coordinates.
+    - y_values: List of y coordinates.
+    - theme: Theme of the plot. Supports 'default' and 'dark'.
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Convert string inputs to lists of integers
+    x = np.array(list(map(int, x_values.split(','))))
+    y = np.array(list(map(int, y_values.split(','))))
+    
+    # Create a meshgrid for x and y values
+    X, Y = np.meshgrid(x, y)
+    Z = X * Y  # Calculate Z values as the product of X and Y
+
+    # Plot a surface
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.Blues)
+
+    if theme == 'dark':
+        plt.style.use('dark_background')
+
+    ax.set_xlabel('X Axis')
+    ax.set_ylabel('Y Axis')
+    ax.set_zlabel('Z Axis')
+    plt.title('3D Surface Graph')
+    plt.savefig('3D_Surface_Graph.png')
+    plt.clf()  # Clear the current figure after saving the graph
